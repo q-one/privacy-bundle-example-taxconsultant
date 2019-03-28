e2c32f204b50d0fe73c50f88153459d6c501105a
Maikel Urlitzki <maikel.urlitzki@q-one.com>
2019-03-28 16:34:37 +0100

    Different views / tabs, personal/finance data

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,6 +11,7 @@ namespace App\Controller;
 use App\Entity\User;
 use Facebook\WebDriver\Exception\ExpectedException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,7 +36,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/edit/{customer}", name="admin_edit")
+     * @Route("/admin/personal/edit/{customer}", name="admin_personal_edit")
      * @param User $customer
      * @param Request $request
      * @return Response
@@ -64,9 +65,50 @@ class AdminController extends AbstractController
 
             $this->addFlash("success", "Customer successfully saved!");
 
-            return $this->redirectToRoute('admin_edit', ['customer' => $customer->getId()]);
+            return $this->redirectToRoute('admin_personal_edit', ['customer' => $customer->getId()]);
         }
 
-        return $this->render("admin/edit.html.twig", ['form' => $form->createView()]);
+        return $this->render(
+            "admin/edit_personal.html.twig",
+            ['form' => $form->createView(), 'customer' => $customer, 'route' => $request->get('_route')]
+        );
+    }
+
+    /**
+     * @Route("/admin/finance/edit/{customer}", name="admin_finance_edit")
+     * @param User $customer
+     * @param Request $request
+     * @return Response
+     */
+    public function editFinanceAction(User $customer, Request $request)
+    {
+        $data = $customer->getCustomerFinanceData();
+
+        $form = $this->createFormBuilder($data)
+            ->add('year', IntegerType::class)
+            ->add('income', IntegerType::class)
+            ->add('expenses', IntegerType::class)
+            ->add('tax_burden', IntegerType::class)
+            ->add('comment', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $this->getDoctrine()->getManager()->persist($data);
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash("success", "Customer successfully saved!");
+
+            return $this->redirectToRoute('admin_finance_edit', ['customer' => $customer->getId()]);
+        }
+
+        return $this->render(
+            "admin/edit_finance.html.twig",
+            ['form' => $form->createView(), 'customer' => $customer, 'route' => $request->get('_route')]
+        );
     }
 }
